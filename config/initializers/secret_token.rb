c82d013acb2618c2ad833fc69403df850d3f5551
Shana Silas <chefshana@me.com>
2013-08-26 22:44:07 -0400

    Initial Commit for BogoMeal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '0baa773110b03be7fe56446c0bf8760edb1a11ac4226e6b8decfc19970e54e9bc0079473d4dfab48fdfc75e7d02d35973ecc38e2405f6df7b1a9bdd6a7043f08'
