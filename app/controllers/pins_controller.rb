9908da5b89e067abcf3b4bcc66724aa0cb82c3d4
Shana Silas <chefshana@me.com>
2013-09-26 23:33:22 -0400

    Added URL Image

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,9 +3,9 @@ class PinsController < ApplicationController
 
   # GET /pins
   # GET /pins.json
+  
   def index
     @pins = Pin.order("created_at desc")
-
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pins }
